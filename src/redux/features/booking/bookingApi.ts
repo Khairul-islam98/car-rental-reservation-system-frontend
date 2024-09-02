302d4039b7249b7d6c06e33fc4fb19d6acc417ca
khairul-islam98 <janeyrahman900@gmail.com>
2024-09-02 10:32:31 +0600

    add redux booking api

---
+++ src/redux/features/booking/bookingApi.ts
@@ -0,0 +1,60 @@
+import { baseApi } from "@/redux/api/baseApi";
+
+const bookingApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    createBooking: builder.mutation({
+      query: (data) => ({
+        url: "/bookings",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["bookings"],
+    }),
+    getAllBookings: builder.query({
+      query: () => ({
+        url: "/bookings",
+        method: "GET",
+      }),
+      providesTags: ["bookings"],
+    }),
+    getUpdateBookings: builder.mutation({
+      query: ({ bookingId, ...data }) => ({
+        url: `/bookings/${bookingId}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["bookings"],
+    }),
+    getMyBooking: builder.query({
+      query: () => ({
+        url: "/bookings/my-bookings",
+        method: "GET",
+      }),
+      providesTags: ["bookings"],
+    }),
+    updateMyBooking: builder.mutation({
+      query: ({ id, ...data }) => ({
+        url: `/bookings/my-bookings/${id}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["bookings"],
+    }),
+    deleteMyBooking: builder.mutation({
+      query: (id) => ({
+        url: `/bookings/my-bookings/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["bookings"],
+    }),
+  }),
+});
+
+export const {
+  useCreateBookingMutation,
+  useGetAllBookingsQuery,
+  useGetUpdateBookingsMutation,
+  useGetMyBookingQuery,
+  useUpdateMyBookingMutation,
+  useDeleteMyBookingMutation,
+} = bookingApi;
